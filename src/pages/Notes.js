e5a8bb25a53f3527ce675ddb315ca95725d8e130
Carlos Teixeira <carlosteixeira@MacBook-Air-de-Carlos.local>
2020-11-13 16:06:32 +0100

    messing with diferent APIs

--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,6 +1,6 @@
 import React, {useState ,useEffect} from 'react';
 
-
+//'https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json'
 
 function Notes() {
   const [error, setError] = useState(null);
@@ -11,12 +11,18 @@ function Notes() {
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/wildcodeschoolparis/datas/master/movies.json')
+    fetch("https://api-football-v1.p.rapidapi.com/v2/events/214226", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "5d5f1c1e61msh1931c908cea9375p1660edjsn672c1ceb6913",
+        "x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+      }
+    })
       .then(res => res.json())
       .then(
         (result) => {
           setIsLoaded(true);
-          setItems(result);
+          setItems(result.headers)
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -38,14 +44,14 @@ function Notes() {
       <div>
     <div className="Notes">
     <p>here i will share my hobbies, interestes and goals for the future</p>
-    <div className="football">fetch an Api or just images</div>
-    <div className="economy">fetch an Api or just images</div>
-    <div className="miniatures">fetch an Api or just images</div>
+    <div className="football">fetch an Api or just images(football)</div>
+    <div className="economy">fetch an Api or just images(economy)</div>
+    <div className="miniatures">fetch an Api or just images(miniatures)</div>
     </div>
   </div>
         {items.map(item => (
           <li key={item.id}>
-            {item.name} {item.price}
+            {item.api.results.events} {item}
           </li>
         ))}
       </ul>
